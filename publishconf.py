4c8b241c25e0fc500446bc085483a8660f60b81c
arturfelipe <arturfelipe.sousa@gmail.com>
2014-05-07 01:58:28 -0300

    Trocando o make html para o make publish no travis.yml para utilizar o publishconf.py e mostrar o SITEURL absoluto

--- publishconf.py
+++ publishconf.py
@@ -10,11 +10,10 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = ''
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+# FEED_ALL_ATOM = 'feeds/all.atom.xml'
+# CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
