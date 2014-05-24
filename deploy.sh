4a378c8570228306dbc050ef49bcf941eec1f776
Fabio C. Barrionuevo da Luz <bnafta@gmail.com>
2014-05-24 13:59:23 -0300

    modifica deploy.sh para limpar ramo master, evitando assim arquivos excluidos ou renomeados

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,8 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
     # go into directory and copy data we're interested in to that directory
     cd built_website
+    # limpa branch master para nao ficar arquivos lixo, como arquivos excluidos e/ou renomeados
+    find . -maxdepth 1 ! -name '.git' ! -name '.*' | xargs rm -rf
     rsync -rv --exclude=.git  ../$PELICAN_OUTPUT_FOLDER/* .
     # add, commit and push files
     git add -f .
